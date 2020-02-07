9620a534b130168f7ca3e8a4e8d5694f0a44b5c5
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2020-02-08 04:15:28 +0900

    Slide show

--- app/javascript/components/components/SimpleSlider.js
+++ app/javascript/components/components/SimpleSlider.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import Slider from 'react-slick';
 import * as slidertypes from '../constants/Initialize';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.scss";
+import "slick-carousel/slick/slick-theme.scss";
 
 export default class SimpleSlider extends Component {
   render() {
     return (
-      <Slider {...slidertypes.SliderSetting}>
-        <div>1</div>
-        <div>2</div>
-      </Slider>
+      <div className="container">
+        <Slider {...slidertypes.SliderSetting}>
+          <div>1</div>
+          <div>2</div>
+        </Slider>
+      </div>
     )
   }
 }
