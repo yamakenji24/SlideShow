795d7d41a42a50b316e894a03f10fe6e321d5f65
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2020-03-17 01:58:14 +0900

    geometryのrender

---
+++ app/javascript/components/animations/renderScene.js
@@ -0,0 +1,7 @@
+
+//var renderScene = function(renderer, scene, camera) {
+export function renderScene(renderer, scene, camera) {
+  return renderer.render(scene, camera)
+}
+//export default renderScene;
+
